63b202387297bc6b1859356fe2d955782d12654e
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-02-17 14:04:20 +0300

    feat: add check fuction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stec = [];
+  stec.push(str[0]);
+  
+  for (let i in bracketsConfig) {
+    if ((stec === bracketsConfig[i][1] && bracketsConfig[i][1] !== bracketsConfig[i][0]) || str.length % 2 !== 0 ) {
+      return false;
+    }
+  }
+
+  for (let i = 1; i < str.length; i++) {
+    for (let j in bracketsConfig) {
+      var ind = bracketsConfig[j].indexOf(str[i]);
+      if (ind !== -1) {
+        ind = ind;
+        break;
+      }
+    }
+
+    if (ind === 0) {
+      let che = stec[stec.length - 1];
+      let k = 0;
+      
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (str[i] === che && bracketsConfig[j][1] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][0]) {
+          stec.pop();
+          k++;
+        }
+      }
+      
+      if (k === 0) {
+        stec.push(str[i]);
+      }
+
+    } else if (ind === 1) {
+      let ch = stec[stec.length - 1];
+      
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (bracketsConfig[j].indexOf(str[i]) === 1 && bracketsConfig[j].indexOf(ch) === 0 ) {
+          stec.pop();
+          j = bracketsConfig.length;
+        }
+      }
+    }
+  }
+
+  if (stec.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
